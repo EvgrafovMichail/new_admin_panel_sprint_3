0070e0f982a0b7b58e0b8a3503f18eb534698ca0
EvgrafovMichail <evgrafovmd@gmail.com>
2024-04-27 12:00:51 +0300

    Add CSRF_TRUSTED_ORIGINS into admin-panel settings

--- docker_compose/movies_admin/config/components/security.py
+++ docker_compose/movies_admin/config/components/security.py
@@ -4,6 +4,7 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = [f"http://{host}:8000" for host in ALLOWED_HOSTS]
 
 
 def _show_toolbar_callback(*_) -> bool:
